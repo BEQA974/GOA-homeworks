f677203fb6aa09a26fbd23a2dd3b03c7b45b09ed
BEQA974 <beqaograpishvili@gmail.com>
2024-09-29 21:46:26 +0400

    first commit

---
+++ Day 2/lesson2.py
@@ -0,0 +1,13 @@
+name = "beka"
+
+surname = "ograpishvili"
+
+age = 15
+
+height = 164.5
+
+
+
+
+
+print("i am" + " " + name + " " + " my surname is " + surname + " " + "i am" + " " + str(age) + " " + "years old")
