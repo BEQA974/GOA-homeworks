9e6ff4cd56a7613797b314f032f505f5c4c76997
BEQA974 <beqaograpishvili@gmail.com>
2024-10-11 23:23:04 +0400

    second commit

---
+++ Day 3/Classwork/work2.py
@@ -0,0 +1,7 @@
+name = "beqa"
+surname = "ograpishvili"
+age = 15
+
+print(name)
+print(surname)
+print(age)
---
+++ Day 3/Homework/ცვლადები.py
@@ -0,0 +1,7 @@
+name = "beqa"
+surname = "ograpishvili"
+age = 15
+height = 164.5
+weight = 60
+
+print("i am" + " " + name + " " + " my surname is " + surname + " " + "i am" + " " + str(age) + " " + "years old" + " " + "my height is" + " " + str(height) + " " + "and my weight is" + " " + str(weight))
