a38f59c1af9c66d0edaa0903f40dfccac2d349c9
BEQA974 <beqaograpishvili@gmail.com>
2024-11-09 15:59:32 +0400

    second commit

---
+++ Day 11/Classwork/Work1.py
@@ -0,0 +1,8 @@
+num1 =int(input("num1: "))
+num2 =float(input("num2: "))
+num3=int(input("num3: "))
+
+print(num1)
+print(num2)
+print(num3)
+
---
+++ Day 11/Homework/Hm7.py
@@ -0,0 +1,15 @@
+num1=int(input("ჩაწერე რიცხვი: "))
+num2=int(input("ჩაწერე რიცხვი: "))
+num3=int(input("ჩაწერე რიცხვი: "))
+
+ჯამი=(num1+num2+num3)
+print(ჯამი)
+
+სხვაობა=(num1-num2-num3)
+print(სხვაობა)
+
+ნამრავლი=(num1*num2*num3)
+print(ნამრავლი)
+
+განაყოფი=(num1/num2/num3)
+print(განაყოფი)
