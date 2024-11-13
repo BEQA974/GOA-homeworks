09cceb5bf5b10e79ea5ca59f06e4ed62c8a426db
BEQA974 <beqaograpishvili@gmail.com>
2024-11-13 23:51:17 +0400

    second commit

---
+++ Day 14/Classwork/Cw1.py
@@ -0,0 +1,10 @@
+print(True or False) #true
+print(True or True) #true
+print(False or False) #false
+print(False or True) #true
+
+print(True and False) #false
+print(True and True) #true
+print(False and False) #false
+print(False and True) #false
+
