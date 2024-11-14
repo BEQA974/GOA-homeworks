b156df943b2d380a5885d004819ec4ad3679dc87
BEQA974 <beqaograpishvili@gmail.com>
2024-11-14 13:13:22 +0400

    second commit

---
+++ Day 14/Homework/Hm2.py
@@ -0,0 +1,17 @@
+ცვლადი=False
+შედარება=69!=56
+print(ცვლადი or შედარება)
+print(True and False or 7>2)
+print(False or 90==45)
+print(True or False and 56>6 and 4!=3)
+print(False and 78==56 or True and 89<23)
+
+ცვლადი1=True
+ცვლადი=False
+შედარება1=45>78
+print(შედარება1 and ცვლადი or ცვლადი1)
+print(True or False)
+print(67<97 or True or False)
+print(False and 98==77)
+print(True and False or 51>8)
+
